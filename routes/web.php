00ae9c748650df2cbd924127aacdaaae454c81e8
Nicolas Suarez <nasuarezro@unal.edu.co>
2024-07-26 13:52:46 -0500

    web routes

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,20 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return (redirect('/admin'));
+});
+
+Route::get('/login', function () {
+    return (redirect('/admin'));
+});
+
+// ruta para logout
+Route::get('/logout', function () {
+    Auth::logout();
+    return (redirect('/admin'));
+});
+
+Route::get('admin/logout', function () {
+    Auth::logout();
+    return (redirect('/admin'));
 });
